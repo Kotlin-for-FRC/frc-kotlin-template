e9c858627a9bd23eadb3404c62a4e74b55a56462
Jade Turner <spacey-sooty@proton.me>
2024-09-11 20:45:51 +0800

    Add langauge to usage reporting

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -4,13 +4,18 @@
 package frc.robot
 
 import edu.wpi.first.wpilibj.TimedRobot
+import edu.wpi.first.hal.FRCNetComm.tInstances;
+import edu.wpi.first.hal.FRCNetComm.tResourceType;
+import edu.wpi.first.hal.HAL
 import edu.wpi.first.wpilibj2.command.CommandScheduler
 import edu.wpi.first.wpilibj2.command.PrintCommand
 
 class Robot : TimedRobot() {
     private val autonomousCommand = PrintCommand("Hello, autonomous!")
 
-    init {}
+    init {
+        HAL.report(tResourceType.kResourceType_Language, tInstances.kLanguage_Kotlin)
+    }
 
     override fun robotPeriodic() {
         CommandScheduler.getInstance().run()
