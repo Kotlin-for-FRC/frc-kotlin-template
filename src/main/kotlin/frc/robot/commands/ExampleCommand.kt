a402f4fe486dfb87ee29c601acd28f8fe3f5603a
Daniel Chen <Daniel_Chen@caryacademy.org>
2024-09-15 15:53:51 -0400

    Removed singleton example

--- src/main/kotlin/frc/robot/commands/ExampleCommand.kt
+++ src/main/kotlin/frc/robot/commands/ExampleCommand.kt
@@ -1,7 +1,6 @@
 package frc.robot.commands
 
 import edu.wpi.first.wpilibj2.command.Command
-import frc.robot.subsystems.ExampleSingletonSubsystem
 import frc.robot.subsystems.ExampleSubsystem
 
 
@@ -15,13 +14,11 @@ import frc.robot.subsystems.ExampleSubsystem
 class ExampleCommand(private val subsystem: ExampleSubsystem): Command() {
     init {
         // Use addRequirements() here to declare subsystem dependencies.
-        addRequirements(subsystem, ExampleSingletonSubsystem)
+        addRequirements(subsystem)
     }
 
     // Called when the command is initially scheduled.
-    override fun initialize() {
-        println(ExampleSingletonSubsystem.exampleProperty)
-    }
+    override fun initialize() {}
 
     // Called every time the scheduler runs while the command is scheduled.
     override fun execute() {}
