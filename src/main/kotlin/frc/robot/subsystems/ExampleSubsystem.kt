9a06994703e239c70044e486d45ec9ddfd8c6694
Daniel Chen <Daniel_Chen@caryacademy.org>
2024-09-16 11:15:06 -0400

    Simplified ExampleSubsystem

--- src/main/kotlin/frc/robot/subsystems/ExampleSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/ExampleSubsystem.kt
@@ -3,8 +3,9 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot.subsystems
 
-import edu.wpi.first.wpilibj.DigitalInput
+import edu.wpi.first.math.system.plant.DCMotor
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax
+import edu.wpi.first.wpilibj.simulation.DCMotorSim
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
@@ -16,7 +17,6 @@ class ExampleSubsystem(
      * The value of inputID depends on the value passed in
      * when the ExampleSubsystem is created.
      */
-    private val digitalInputID: Int,
     private val motorID: Int,
 ): SubsystemBase() {
     init {
@@ -35,17 +35,7 @@ class ExampleSubsystem(
             println("This is an example command.")
         }
 
-    /**
-     * An example of a regular kotlin property
-     * whose value cannot be changed.
-     */
-    private val digitalInput = DigitalInput(digitalInputID)
     private val motor = PWMSparkMax(motorID)
-
-    /**
-     * An example of a regular kotlin property
-     * that can be modified.
-     */
     private var exampleMutableProperty = 2.0
 
     /**
@@ -55,44 +45,6 @@ class ExampleSubsystem(
      * Properties like these can be used to query a boolean state of the subsystem (for example, a digital sensor).
      */
     val exampleCondition: Boolean get() = motor.isAlive
-
-    /**
-     * An example of a property with a custom getter and a custom setter.
-     *
-     * When this property is assigned a value, the code within the set(){ }
-     * block will also run.
-     *
-     * Note that this property does not store any data,
-     * and is essentially a proxy for 2 different methods.
-     */
-    var exampleGetSetProperty: Double
-        get() = motor.get()
-        set(value) {
-            if (digitalInput.get()) motor.set(value)
-        }
-
-    /**
-     * An example of a getter/setter property with a backing field.
-     *
-     * A backing field acts like an invisible variable tied to the property itself,
-     * which can be accessed in the get() and set() methods of that property.
-     *
-     * In order to use a backing field, an initial value must be given to the property
-     * itself.
-     *
-     * To refer to a backing field, use the keyword "field".
-     *
-     * Properties with a backing field can exclude either the getter or the setter.
-     */
-    var exampleBackingFieldProperty = exampleConstructorArg
-        get() {
-            field++
-            return field
-        }
-        set(value) {
-            motor.stopMotor()
-            field = value + 1
-        }
 
     /**
      * An example subsystem property using a lazy initializer.
@@ -105,7 +57,12 @@ class ExampleSubsystem(
      * giving a simulation object a lazy initializer can prevent it
      * from using unnecessary CPU or memory on the real robot.
      */
-    val lazyProperty: Int by lazy { digitalInput.channel }
+    val lazyProperty by lazy {
+        DCMotorSim(
+            DCMotor.getKrakenX60(1),
+            1.0, 0.004
+        )
+    }
 
     override fun periodic() {
         // This method will be called once per scheduler run
